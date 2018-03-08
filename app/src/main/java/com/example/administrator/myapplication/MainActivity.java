70c8a01afd11caf0c205344305cd6ee9da023550
shiliang117 <shi.liang1976@163.com>
2018-03-08 17:05:14 +0800

    office modify

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.administrator.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void sendMessage(View view){
+        Intent intent=new Intent(this ,DisplayMessageActivity.class);
+        EditText editText=(EditText) findViewById(R.id.edit_message );
+        String message=editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE,message);
+        startActivity(intent);
+    }
+    public final static String EXTRA_MESSAGE="com.example.administrator.MESSAGE";
 }
